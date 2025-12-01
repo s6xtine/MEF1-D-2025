a2b4a7f11e5f4367c6a717404ea4bd61abd21e1a
Assiaelk <assiaelkharroubi@gmail.com>
2025-12-01 10:54:02 +0100

    changement main.changement

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "AVL.h"
 
-int main() {
-    printf("Bonjour, Projet MEF1-D !\n");
-    printf("lol");
-    printf("\n");
-    printf("Ceci est un test de commit.\n");
+int main(){
+    Arbre* racine = NULL;
+    FILE* sortie = fopen("sortie.txt","w"); //ouverture du fichier de sortie trier par le shell
+    if(sortie==NULL){
+        exit(1);
+    }
+    int id;
+    long int capa, conso;
+    int h=-1;
+    while(scanf("%d:%ld:%ld\n", &id, &capa, &conso)==3){
+        racine=insertionAVL(racine, id, &h, capa, conso); //fonction qui insert et calcule la somme
+        //la somme est calculée dans la fonction insertionAVL
+    }
+    parcoursprefixe(racine, sortie); //appel de la fonction qui fait un parcours prefixe
+    freeAVL(racine);
     
     return 0;
 }
