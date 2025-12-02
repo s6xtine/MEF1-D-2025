de2b366e1059ccf16b96d7b9fc06fa2720d1d163
Morgan Lechevallier <morgan@example.com>
2025-12-02 14:29:39 +0100

    histogramme pas trop commence

---
+++ histo.c
@@ -0,0 +1,13 @@
+#include "noeud.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+Noeud* insererAVL(Noeud* racine, Usine u){
+    if(racine != NULL){
+        Noeud* nouveau = malloc(sizeof(Noeud));
+        
+    }
+    
+
+}
